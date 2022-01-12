b7701904a154abe2586f0251444498f8307b239e
smori <shinichiro.mori.1983@gmail.com>
2022-01-13 01:21:17 +0900

    example: update plugin config based on current spec

--- example/.vuepress/config.js
+++ example/.vuepress/config.js
@@ -18,6 +18,9 @@ module.exports = {
       onlyFirstAndLastCommit: false,
     }],
 
-    require('../../src'),
+    [require('../../src'), {
+      useGlobalUi: true,
+      usePageBlock: true,
+    }],
   ],
 };
