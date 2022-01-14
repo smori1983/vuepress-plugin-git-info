9b81dfac5d61bff21ea4ea0e6aaffb68b4ad94ed
smori <shinichiro.mori.1983@gmail.com>
2022-01-15 00:09:37 +0900

    Follow style of theme-default

--- src/components/GlobalUi.vue
+++ src/components/GlobalUi.vue
@@ -3,8 +3,10 @@
     class="plugin-git-info"
     v-if="git"
   >
-    <div class="label">Git info of this page</div>
-    <git-info v-bind:git="git"></git-info>
+    <div class="plugin-git-info-content">
+      <div class="label">Git info of this page</div>
+      <git-info v-bind:git="git"></git-info>
+    </div>
   </div>
 </template>
 
@@ -40,13 +42,30 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-.plugin-git-info {
-  max-width: $contentWidth
-  padding-left: $sidebarWidth
-  margin: 0 auto
+/**
+ * See: @vuepress/theme-default/styles/index.styl
+ */
+@require '../styles/wrapper.styl'
 
-  .label {
-    font-weight: bold
-  }
-}
+/**
+ * See: @vuepress/theme-default/styles/mobile.styl
+ */
+$mobileSidebarWidth = $sidebarWidth * 0.82
+
+.plugin-git-info
+  padding-left $sidebarWidth
+
+  .plugin-git-info-content
+    @extend $wrapper
+
+    .label
+      font-weight bold
+
+@media (max-width: $MQNarrow)
+  .plugin-git-info
+    padding-left $mobileSidebarWidth
+
+@media (max-width: $MQMobile)
+  .plugin-git-info
+    padding-left 0
 </style>
